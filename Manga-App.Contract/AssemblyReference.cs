233597d05573ef52be574d7bbff569e5e9bc797f
NDViet2002 <nguyendinh.viet2002np@gmail.com>
2025-02-12 07:54:37 +0700

    Add project files.

---
+++ Manga-App.Contract/AssemblyReference.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+namespace Manga_App.Contract;
+public class AssemblyReference
+{
+
+    public static readonly Assembly Assembly = typeof(AssemblyReference).Assembly;
+}
---
+++ Manga-App.Domain/AssemblyReference.cs
@@ -0,0 +1,9 @@
+﻿using System.Reflection;
+
+namespace Manga_App.Domain;
+
+public class AssemblyReference
+{
+
+    public static readonly Assembly Assembly = typeof(AssemblyReference).Assembly;
+}
