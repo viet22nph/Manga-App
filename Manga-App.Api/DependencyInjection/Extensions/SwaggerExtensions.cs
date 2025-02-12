9ff4078f1a080631ff32a5c7ac79d4e8bef68657
NDViet2002 <nguyendinh.viet2002np@gmail.com>
2025-02-12 09:49:30 +0700

    Configure Swagger with API versioning

---
+++ Manga-App.Api/DependencyInjection/Extensions/SwaggerExtensions.cs
@@ -0,0 +1,54 @@
+﻿using MangaApp.Api.DependencyInjection.Options;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Swashbuckle.AspNetCore.SwaggerUI;
+
+namespace MangaApp.Api.DependencyInjection.Extensions;
+public static class SwaggerExtensions
+{
+    public static void AddSwagger(this IServiceCollection services)
+    {
+        services.AddSwaggerGen(options =>
+        {
+            options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme { Description = "JWT Authorization here !", Name = "Authorization", In = ParameterLocation.Header, Scheme = "Bearer" });
+            options.AddSecurityRequirement(new OpenApiSecurityRequirement() {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Id= "Bearer",
+                                Type = ReferenceType.SecurityScheme,
+                            },
+                            Scheme ="oauth2",
+                            Name ="Bearer",
+                            In = ParameterLocation.Header
+                        },
+                        new List<string>()
+                    }
+                });
+        });
+        services.AddTransient<IConfigureOptions<SwaggerGenOptions>, ConfigureSwaggerOptions>();
+    }
+    public static void ConfigureSwagger(this WebApplication app)
+    {
+        var apiVersionDescriptionProvider = app.Services.GetRequiredService<IApiVersionDescriptionProvider>();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseSwagger();
+            //app.UseSwaggerUI();
+            app.UseSwaggerUI(options =>
+            {
+                foreach (var description in apiVersionDescriptionProvider.ApiVersionDescriptions.Reverse())
+                {
+                    options.SwaggerEndpoint($"/swagger/{description.GroupName}/swagger.json",
+                        description.GroupName.ToUpperInvariant());
+                }
+            });
+        }
+    }
+}
--- Manga-App.Api/Program.cs
+++ Manga-App.Api/Program.cs
@@ -1,3 +1,7 @@
+using MangaApp.Api.DependencyInjection.Extensions;
+using MangaApp.Persistence.DependencyInjections.Extentions;
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -7,7 +11,38 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+             policy.AllowAnyOrigin()
+                   .AllowAnyMethod()
+                   .AllowAnyHeader();
+        });
+});
+builder.Services.AddApiVersioning(o => {
+    o.ReportApiVersions = true;
+    o.AssumeDefaultVersionWhenUnspecified = true;
+    o.DefaultApiVersion = new ApiVersion(1, 0);
+});
+builder.Services.AddVersionedApiExplorer(setup =>
+{
+    setup.GroupNameFormat = "'v'VVV";
+    setup.SubstituteApiVersionInUrl = true;
+});
+
+builder.Services.AddSwagger();
+
 var app = builder.Build();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.ConfigureSwagger();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -20,6 +55,11 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.UseAuthentication();
+
+app.UseStaticFiles();
+
+app.UseCors(MyAllowSpecificOrigins).UseForwardedHeaders();
 app.MapControllers();
 
 app.Run();
